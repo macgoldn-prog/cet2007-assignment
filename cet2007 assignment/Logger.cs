6126d5595f76a76f63cb07f16b49b86f85329b01
macgoldn-prog <mac.goldn@gmail.com>
2025-11-27 11:32:02 +0000

    Deleted cet2007 assignment/Logger.cs
    logger not functioning as intended, deleted to redo

--- cet2007 assignment/Logger.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-internal class Logger
-{
-    // Placeholder for future logging implementation
-    // Important actions should be logged with timestamps, such as adding/removing games or updating player stats
-
-}
