81c1d791bb4d22c8088ecc25abe26be22c6f63c0
bi79fd <bi79fd@FOT-CELLB21>
2025-11-27 10:17:18 +0000

    added encapsulation for GameLibrary class

---
+++ cet2007 assignment/GameLibrary.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CET2007_Assignment
+{
+    internal class GameLibrary : IComparable<GameLibrary>
+    {
+        // Public read-only properties
+        public string Name { get; }
+        public string Genre { get; }
+        public int Year { get; }
+
+        public GameLibrary(string sName, string sGenre, int iYear)
+        {
+            Name = sName;
+            Genre = sGenre;
+            Year = iYear;
+        }
+
+        // Implement CompareTo to satisfy IComparable<T>
+        public int CompareTo(GameLibrary other)
+        {
+            if (other == null) return 1;
+            return string.Compare(Name, other.Name, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public override string ToString() => Name;
+    }
+}
--- cet2007 assignment/Program.cs
+++ cet2007 assignment/Program.cs
@@ -150,29 +150,7 @@ namespace CET2007_Assignment
 
 
         // GameLibrary class representing a game in the library
-        class GameLibrary : IComparable<GameLibrary>
-        {
-            // Public read-only properties
-            public string Name { get; }
-            public string Genre { get; }
-            public int Year { get; }
 
-            public GameLibrary(string sName, string sGenre, int iYear)
-            {
-                Name = sName;
-                Genre = sGenre;
-                Year = iYear;
-            }
-
-            // Implement CompareTo to satisfy IComparable<T>
-            public int CompareTo(GameLibrary other)
-            {
-                if (other == null) return 1;
-                return string.Compare(Name, other.Name, StringComparison.OrdinalIgnoreCase);
-            }
-
-            public override string ToString() => Name;
-        }
 
         class SearchFunctionality
         {
