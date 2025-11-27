6003ab8dd6912d9e3b1635e43db19aae0c19c449
bi79fd <bi79fd@FOT-CELLB21>
2025-11-27 14:18:09 +0000

    Attempted applying a singleton pattern and LoggerEntry to compile the Logger.cs file

---
+++ cet2007 assignment/LoggerEntry.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+internal public class LoggerEntry
+{
+    public DateTime Timestamp { get; set; }
+    public string Source { get; set; }
+    public string Message { get; set; }
+    public LoggerEntry(string source, string message)
+    {
+        Source = source;
+        Message = message;
+        Timestamp = DateTime.Now;
+    }
+    public LoggerEntry()
+    {
+    }
+}
