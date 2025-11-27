e3aaafc18dda3cd4a07d0aebac944ccacc94bf59
bi79fd <bi79fd@FOT-CELLB21>
2025-11-27 10:13:59 +0000

    added encapsulation for PlayerStats class

---
+++ cet2007 assignment/PlayerStats.cs
@@ -0,0 +1,23 @@
+﻿namespace CET2007_Assignment
+{
+    internal class PlayerStats
+    {
+        // Public read-only properties so callers can access the data
+        public string PlayerName { get; }
+        public int GamesPlayed { get; }
+        public int TotalScore { get; }
+
+        public int Id { get; }
+
+        public PlayerStats(string sPlayerName, int ID, int iGamesPlayed, int iTotalScore)
+        {
+            PlayerName = sPlayerName;
+            GamesPlayed = iGamesPlayed;
+            TotalScore = iTotalScore;
+            Id = ID;
+        }
+
+
+        public override string ToString() => PlayerName;
+    }
+}
--- cet2007 assignment/Program.cs
+++ cet2007 assignment/Program.cs
@@ -147,26 +147,7 @@ namespace CET2007_Assignment
 
         }
 
-        class PlayerStats
-        {
-            // Public read-only properties so callers can access the data
-            public string PlayerName { get; }
-            public int GamesPlayed { get; }
-            public int TotalScore { get; }
 
-            public int Id { get; }
-
-            public PlayerStats(string sPlayerName, int ID, int iGamesPlayed, int iTotalScore)
-            {
-                PlayerName = sPlayerName;
-                GamesPlayed = iGamesPlayed;
-                TotalScore = iTotalScore;
-                Id = ID;
-            }
-
-
-            public override string ToString() => PlayerName;
-        }
 
         // GameLibrary class representing a game in the library
         class GameLibrary : IComparable<GameLibrary>
