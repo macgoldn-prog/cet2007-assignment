e97201ba6d47fae5c6f23c3215c921d4e2b7dace
bi79fd <bi79fd@FOT-CELLB21>
2025-11-27 10:20:14 +0000

    added encapsulation for Admin class

--- cet2007 assignment/Program.cs
+++ cet2007 assignment/Program.cs
@@ -171,44 +171,5 @@ namespace CET2007_Assignment
             public DataPersistence() { }
         }
 
-        class Admin
-        {
-            // Add and remove games from library
-
-            public void ManageGameLibrary()
-            {
-                Console.WriteLine("--- Admin: Manage Game Library ---");
-                Console.WriteLine("1. Add Game");
-                Console.WriteLine("2. Remove Game");
-                Console.Write("Enter choice: ");
-                string adminChoice = Console.ReadLine();
-                if (adminChoice == "1")
-                {
-                    // Add game logic
-                    Console.Write("Enter game name: ");
-                    string name = Console.ReadLine();
-                    Console.Write("Enter game genre: ");
-                    string genre = Console.ReadLine();
-                    Console.Write("Enter game year: ");
-                    int year = int.Parse(Console.ReadLine());
-                    // Add the new game to the library (not implemented)
-                    Console.WriteLine($"Game '{name}' added to the library.");
-                }
-                else if (adminChoice == "2")
-                {
-                    // Remove game logic
-                    Console.Write("Enter game name to remove: ");
-                    string nameToRemove = Console.ReadLine();
-                    // Remove the game from the library (not implemented)
-                    Console.WriteLine($"Game '{nameToRemove}' removed from the library.");
-                }
-                else
-                {
-                    Console.WriteLine("Invalid choice.");
-                }
-            }
-            // Update player statistics
-            // Add new players
-        }
     }
 }
