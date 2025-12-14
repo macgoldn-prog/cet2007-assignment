f7c0aa3890034501c5bfb0ea604c401396d7d6ec
macgoldn-prog <mac.goldn@gmail.com>
2025-12-14 13:38:34 +0000

    added unit test for logger (LoggerTests.cs) and breakpoint line 43

---
+++ CET2007ASSIGNMENTTESTS/CET2007ASSIGNMENTTESTS/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CET2007ASSIGNMENTTESTS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CET2007ASSIGNMENTTESTS")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3f17f06f-1e49-4280-ae3e-2f60fc26fad7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
