9a5550c6385276ab45a92874587cf9eba644f591
macgoldn-prog <mac.goldn@gmail.com>
2025-12-16 00:42:45 +0000

    successful unit test added for LOGGER. Checks for same instance multiple times. (finally works wooo)

---
+++ CET2007Assignment_Tests/LoggerTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Reflection;
+using CET2007_Assignment;
+
+
+namespace CET2007Assignment_Tests
+{
+    [TestClass]
+    public class LoggerTests
+    {
+
+        [TestMethod]
+        public void Logger_GetInstance_IsSingleton()
+        {
+
+            // checks if the same instance is returned more than once
+            var first = Logger.GetInstance();
+            var second = Logger.GetInstance();
+
+            Assert.IsNotNull(first);
+            Assert.AreSame(first, second);
+        }
+    }
+}
